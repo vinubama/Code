b16bcec2a598d10f9673124b3a41ba83233603b5
vinubama <vinukrishna1312@gmail.com>
2023-06-29 21:23:00 +0530

    [Easy] Add solution for CutTheSticks hackerrank problem

---
+++ easy/CutTheSticks/solution.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'cutTheSticks' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def cutTheSticks(arr):
+    res = []
+    while arr:
+        res.append(len(arr))
+        arr =[i-min(arr) for i in arr if (i-min(arr)) > 0]
+    return res
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
+
